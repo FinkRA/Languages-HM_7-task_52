ab1703ec78ac3bc08e715f6017b3d0eaafafc0a2
Rudolf Fink <rudolf.baldr.fink@gmail.com>
2022-12-18 04:14:07 +0500

    done

--- Program.cs
+++ Program.cs
@@ -5,3 +5,33 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите чисол сторк массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чисол столбцов массива: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[x, y];
+
+var element = new Random();
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        array[i, j] = element.Next(-1 * 10, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int j = 0; j < y; j++)
+{
+    double summ = 0;
+    for (int i = 0; i < x; i++)
+    {
+        summ = summ + array[i, j];
+    }
+    double average = summ / x;
+    Console.WriteLine($"Среднее арифметическое {j + 1}-го столбца = {Math.Round(average, 3)} ");
+}
